356c35b72b03f64b8e4fe344b5a4f4102a5ef10e
WillSolow <146394143+solowwill@users.noreply.github.com>
2024-03-14 09:59:39 -0700

    renaming folders like a bash god

--- base_train.py
+++ base_train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-# model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+#model = YOLO("deep_learnin/multitask/yolov8-cls.yaml")  # build a new model from scratch
+model = YOLO("yolov8n-cls.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="deep_learnin/coco126_kyle_noise.yaml", project="deep_learnin", batch=50, epochs=100)  # train the model
+model.train(data="deep_learnin/coco126_will_multi_class.yaml", project="deep_learnin", batch=50, epochs=100)  # train the model
 # metrics = model.val()  # evaluate model performance on the validation set
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 # path = model.export(format="onnx")  # export the model to ONNX format
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-# model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("deep_learnin/coco_126_stop/weights/best.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n-classify.yaml")  # build a new model from scratch
+#model = YOLO("deep_learnin/coco_126_stop/weights/best.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-# model.train(data="coco128.yaml", project="deep_learnin", batch=32, epochs=100)  # train the model
-metrics = model.val(data="/home/kyle/adversarial_deep_learning_v8/deep_learnin/coco126_kyle_noise.yaml")  # evaluate model performance on the validation set
+model.train(data="coco128.yaml", project="deep_learnin", batch=32, epochs=100)  # train the model
+#metrics = model.val(data="/home/kyle/adversarial_deep_learning_v8/deep_learnin/coco126_kyle_noise.yaml")  # evaluate model performance on the validation set
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 # path = model.export(format="onnx")  # export the model to ONNX format
